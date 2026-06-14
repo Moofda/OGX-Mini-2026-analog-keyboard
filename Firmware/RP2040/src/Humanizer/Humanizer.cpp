0068093b9faa85e5d706aa986b6519faa9da417c
Moofda <mikejshawn@protonmail.com>
2026-06-14 02:05:39 -0700

    Update Humanizer.cpp

--- Firmware/RP2040/src/Humanizer/Humanizer.cpp
+++ Firmware/RP2040/src/Humanizer/Humanizer.cpp
@@ -6,13 +6,13 @@ static const fix16_t FIX_0         = 0x00000000;
 static const fix16_t FIX_NEG1      = 0xFFFF0000;
 static const fix16_t FIX_095       = 0x0000F333;
 
-Fix16 Humanizer::next_rand()
+fix16_t Humanizer::next_rand()
 {
     rng_state_ ^= rng_state_ << 13;
     rng_state_ ^= rng_state_ >> 17;
     rng_state_ ^= rng_state_ << 5;
     int16_t signed_val = static_cast<int16_t>(rng_state_ % 1000);
-    return Fix16(signed_val) / Fix16(static_cast<int16_t>(1000));
+    return fix16_div(fix16_from_int(signed_val), fix16_from_int(1000));
 }
 
 void Humanizer::process(Gamepad::PadIn& pad_in)
@@ -38,8 +38,8 @@ void Humanizer::process(Gamepad::PadIn& pad_in)
 
 void Humanizer::process_stick(
     int16_t& x, int16_t& y,
-    Fix16& drift_x, Fix16& drift_y,
-    Fix16& target_x, Fix16& target_y,
+    fix16_t& drift_x, fix16_t& drift_y,
+    fix16_t& target_x, fix16_t& target_y,
     uint32_t& retarget_counter,
     uint32_t& fade_counter,
     bool& was_idle)
@@ -48,18 +48,18 @@ void Humanizer::process_stick(
     fix16_t ny = fix16_div(fix16_from_int(y), FIX_INT16_MAX);
 
     fix16_t mag_sq = fix16_add(fix16_mul(nx, nx), fix16_mul(ny, ny));
-    fix16_t idle_sq = fix16_mul(settings_.idle_threshold.value, settings_.idle_threshold.value);
+    fix16_t idle_sq = fix16_mul(settings_.idle_threshold, settings_.idle_threshold);
     bool is_idle = (mag_sq < idle_sq);
 
     if (!is_idle)
     {
-        fix16_t cap_sq = fix16_mul(settings_.magnitude_cap.value, settings_.magnitude_cap.value);
+        fix16_t cap_sq = fix16_mul(settings_.magnitude_cap, settings_.magnitude_cap);
         if (mag_sq > cap_sq)
         {
             fix16_t mag = fix16_sqrt(mag_sq);
             if (mag > FIX_0)
             {
-                fix16_t scale = fix16_div(settings_.magnitude_cap.value, mag);
+                fix16_t scale = fix16_div(settings_.magnitude_cap, mag);
                 nx = fix16_mul(nx, scale);
                 ny = fix16_mul(ny, scale);
             }
@@ -72,15 +72,15 @@ void Humanizer::process_stick(
         if (retarget_counter >= settings_.drift_retarget_frames)
         {
             retarget_counter = 0;
-            target_x = Fix16(next_rand() * settings_.drift_max);
-            target_y = Fix16(next_rand() * settings_.drift_max);
+            target_x = fix16_mul(next_rand(), settings_.drift_max);
+            target_y = fix16_mul(next_rand(), settings_.drift_max);
         }
-        fix16_t tx = fix16_sub(target_x.value, drift_x.value);
-        fix16_t ty = fix16_sub(target_y.value, drift_y.value);
-        drift_x.value = fix16_add(drift_x.value, fix16_mul(tx, settings_.drift_strength.value));
-        drift_y.value = fix16_add(drift_y.value, fix16_mul(ty, settings_.drift_strength.value));
-        nx = fix16_add(nx, drift_x.value);
-        ny = fix16_add(ny, drift_y.value);
+        fix16_t tx = fix16_sub(target_x, drift_x);
+        fix16_t ty = fix16_sub(target_y, drift_y);
+        drift_x = fix16_add(drift_x, fix16_mul(tx, settings_.drift_strength));
+        drift_y = fix16_add(drift_y, fix16_mul(ty, settings_.drift_strength));
+        nx = fix16_add(nx, drift_x);
+        ny = fix16_add(ny, drift_y);
         if (nx > FIX_1)    nx = FIX_1;
         if (nx < FIX_NEG1) nx = FIX_NEG1;
         if (ny > FIX_1)    ny = FIX_1;
@@ -88,8 +88,8 @@ void Humanizer::process_stick(
     }
     else
     {
-        drift_x.value = fix16_mul(drift_x.value, FIX_095);
-        drift_y.value = fix16_mul(drift_y.value, FIX_095);
+        drift_x = fix16_mul(drift_x, FIX_095);
+        drift_y = fix16_mul(drift_y, FIX_095);
         retarget_counter = 0;
     }
 
