c1b1eba34c057be6c1d45a05cbc2a863991b9513
Moofda <mikejshawn@protonmail.com>
2026-06-14 01:59:19 -0700

    Update Humanizer.h

--- Firmware/RP2040/src/Humanizer/Humanizer.h
+++ Firmware/RP2040/src/Humanizer/Humanizer.h
@@ -7,14 +7,14 @@
 
 struct HumanizerSettings
 {
-    Fix16 magnitude_cap            = Fix16(0.85f);
-    Fix16 magnitude_noise          = Fix16(0.03f);
-    Fix16 drift_strength           = Fix16(0.08f);
-    Fix16 drift_max                = Fix16(0.25f);
-    uint32_t drift_retarget_frames = 400;
-    Fix16 idle_threshold           = Fix16(0.10f);
-    uint32_t release_fade_frames   = 12;
-    bool enabled                   = true;
+    fix16_t magnitude_cap            = 0x0000D999; // 0.85
+    fix16_t magnitude_noise          = 0x00000799; // 0.03
+    fix16_t drift_strength           = 0x00001470; // 0.08
+    fix16_t drift_max                = 0x00004000; // 0.25
+    uint32_t drift_retarget_frames   = 400;
+    fix16_t idle_threshold           = 0x00001999; // 0.10
+    uint32_t release_fade_frames     = 12;
+    bool enabled                     = true;
 };
 
 class Humanizer
@@ -31,21 +31,21 @@ public:
 private:
     HumanizerSettings settings_;
 
-    Fix16 drift_lx_, drift_ly_;
-    Fix16 drift_rx_, drift_ry_;
-    Fix16 target_lx_, target_ly_;
-    Fix16 target_rx_, target_ry_;
+    fix16_t drift_lx_ = 0, drift_ly_ = 0;
+    fix16_t drift_rx_ = 0, drift_ry_ = 0;
+    fix16_t target_lx_ = 0, target_ly_ = 0;
+    fix16_t target_rx_ = 0, target_ry_ = 0;
     uint32_t retarget_counter_l_ = 0, retarget_counter_r_ = 0;
     uint32_t fade_counter_l_ = 0, fade_counter_r_ = 0;
     bool was_idle_l_ = true, was_idle_r_ = true;
     uint32_t rng_state_ = 12345;
 
-    Fix16 next_rand();
+    fix16_t next_rand();
 
     void process_stick(
         int16_t& x, int16_t& y,
-        Fix16& drift_x, Fix16& drift_y,
-        Fix16& target_x, Fix16& target_y,
+        fix16_t& drift_x, fix16_t& drift_y,
+        fix16_t& target_x, fix16_t& target_y,
         uint32_t& retarget_counter,
         uint32_t& fade_counter,
         bool& was_idle);
