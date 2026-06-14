f787c8935d8c66132adc2ba8c44cbbaaa907a780
Moofda <mikejshawn@protonmail.com>
2026-06-14 01:08:46 -0700

    Update Humanizer.h

--- Firmware/RP2040/src/Humanizer/Humanizer.h
+++ Firmware/RP2040/src/Humanizer/Humanizer.h
@@ -3,15 +3,16 @@
 
 #include <cstdint>
 #include "Gamepad/Gamepad.h"
+#include "libfixmath/fix16.hpp"
 
 struct HumanizerSettings
 {
-    float magnitude_cap            = 0.85f;
-    float magnitude_noise          = 0.03f;
-    float drift_strength           = 0.08f;
-    float drift_max                = 0.25f;
+    Fix16 magnitude_cap            = Fix16(0.85f);
+    Fix16 magnitude_noise          = Fix16(0.03f);
+    Fix16 drift_strength           = Fix16(0.08f);
+    Fix16 drift_max                = Fix16(0.25f);
     uint32_t drift_retarget_frames = 400;
-    float idle_threshold           = 0.10f;
+    Fix16 idle_threshold           = Fix16(0.10f);
     uint32_t release_fade_frames   = 12;
     bool enabled                   = true;
 };
@@ -30,22 +31,21 @@ public:
 private:
     HumanizerSettings settings_;
 
-    float drift_lx_ = 0.0f, drift_ly_ = 0.0f;
-    float drift_rx_ = 0.0f, drift_ry_ = 0.0f;
-    float target_lx_ = 0.0f, target_ly_ = 0.0f;
-    float target_rx_ = 0.0f, target_ry_ = 0.0f;
+    Fix16 drift_lx_, drift_ly_;
+    Fix16 drift_rx_, drift_ry_;
+    Fix16 target_lx_, target_ly_;
+    Fix16 target_rx_, target_ry_;
     uint32_t retarget_counter_l_ = 0, retarget_counter_r_ = 0;
     uint32_t fade_counter_l_ = 0, fade_counter_r_ = 0;
     bool was_idle_l_ = true, was_idle_r_ = true;
     uint32_t rng_state_ = 12345;
 
-    float next_rand();
-    float next_rand_pos();
+    Fix16 next_rand();
 
     void process_stick(
         int16_t& x, int16_t& y,
-        float& drift_x, float& drift_y,
-        float& target_x, float& target_y,
+        Fix16& drift_x, Fix16& drift_y,
+        Fix16& target_x, Fix16& target_y,
         uint32_t& retarget_counter,
         uint32_t& fade_counter,
         bool& was_idle);
